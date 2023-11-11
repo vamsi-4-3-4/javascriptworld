2c8d7d692ccd713a729d79e611d7a693d7514f14
vamsi-4-3-4 <vamsiece101@gmail.com>
2023-11-12 01:09:11 +0530

    dot and bracket notation added

---
+++ jsobjects/dotandbracketnotation.js
@@ -0,0 +1,22 @@
+let spaceshiptomars={
+name:"Space_Ship_oreo",
+owner:"Vamsi",
+speedExpected:5,
+
+
+
+develop:function(){
+console.log(`
+${this.owner} developed a spaceship 
+with ${this.speedExpected} lakh km/hr
+and its name is:${this.name}
+`)
+},
+
+
+
+
+}
+
+console.log(spaceshiptomars.name)
+console.log(spaceshiptomars['name'])
